e753124a1db9161637b95343d5f4290c8d02eae8
Andrey Efimov <efiand@ya.ru>
2022-11-24 17:32:30 +0300

    Как установить twig

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+import browser from "browser-sync";
+import getData from "gulp-data";
+import gulp from "gulp";
+import twig from "gulp-twig";
+
+const buildHtml = () =>
+  gulp
+    .src("source/layouts/pages/**/*.twig")
+    .pipe(
+      getData(({ path }) => {
+        const page = path
+          .replace(/^.*pages(\\+|\/+)(.*)\.twig$/, "$2")
+          .replace(/\\/g, "/");
+
+        return {
+          page,
+        };
+      })
+    )
+    .pipe(twig())
+    .pipe(gulp.dest("source"));
+
+const reload = (done) => {
+  browser.reload();
+  done();
+};
+
+const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: "source",
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+};
+
+const watcher = () => {
+  gulp.watch("source/layouts/**/*.twig", gulp.series(buildHtml, reload));
+};
+
+export default gulp.series(buildHtml, server, watcher);
